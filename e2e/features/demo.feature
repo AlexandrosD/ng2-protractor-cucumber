093b6385acd8ac126df3713b1c2102d0a77b3600
Alexandros Dallas <alexandros.dallas@trasysgroup.com>
2016-07-03 15:35:52 +0300

    refactoring for cucumber-tsflow

--- e2e/features/demo.feature
+++ e2e/features/demo.feature
@@ -3,17 +3,17 @@ Feature: This is a demo feature
     Scenario: The user is showcasing a demo feature
 
         Given I open the browser
-        When I visint the page
-        Then it should display a the message "app works"
+        When I visit the page
+        Then it should display a the message 'app works!'
 
 
     Scenario Outline: The user is showcasing a data-driven demo
 
         Given I open the browser
-        When I visint the page
-        Then it should display a the message "<message>"
+        When I visit the page
+        Then it should display a the message '<message>'
     
     Examples:
       | message           |
-      | App works         |
+      | app works!        |
       | App does not work |
--- e2e/features/demo.steps.ts
+++ e2e/features/demo.steps.ts
@@ -1,26 +1,30 @@
+import {binding, given, when, then} from 'cucumber-tsflow';
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 var expect = chai.use(chaiAsPromised).expect;
 import { BoilerplateBddPage } from '../app.po';
 
-var steps = function steps() {
-
-  var page;
+@binding()
+class DemoSteps {
   
-  cucumber.Given('/^I open the browser$/', function(callback) {
-    page = new BoilerplateBddPage();
+  private page;
+
+  @given(/^I open the browser$/)
+  private givenIOpenTheBrowser(callback: cucumber.CallbackStepDefinition): void {
+    this.page = new BoilerplateBddPage();
     callback();
-  });
+  };
 
-  cucumber.When('/^I visit the page$/', function(callback) {
-    page.navigateTo();
+  @when(/^I visit the page$/)
+  private whenIVisitThePage(callback: cucumber.CallbackStepDefinition): void {
+    this.page.navigateTo();
     callback();
-  });
+  };
 
-  cucumber.Then('/^Then it should display a the message "(.+))"$/', function(text: string, callback) {
-    expect(page.getParagraphText()).to.eventually.equal(text).and.notify(callback);
-  }); 
-
+  @then(/^it should display a the message '(.+)'$/)
+  private thenAssertTitle(text: string, callback: cucumber.CallbackStepDefinition): void {
+    expect(this.page.getParagraphText()).to.eventually.equal(text).and.notify(callback);
+  };
 }
 
-export = steps;
+export = DemoSteps;
