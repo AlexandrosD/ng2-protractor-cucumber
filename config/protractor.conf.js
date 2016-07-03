3b9bdd11e71fa2eb7be49db185dfb533bd703811
Alexandros Dallas <alexandros.dallas@trasysgroup.com>
2016-07-03 14:20:58 +0300

    Update protractor configuration - Replace Jasmine with Cucumber

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -1,32 +1,34 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/docs/referenceConf.js
 
-/*global jasmine */
-var SpecReporter = require('jasmine-spec-reporter');
-
 exports.config = {
+
+  framework: 'custom',
+	frameworkPath: require.resolve('protractor-cucumber-framework'),
+	useAllAngular2AppRoots: true,
+	specs: [
+		'../e2e/**/*.feature'
+	],
+	cucumberOpts: {
+		require: [
+			'../e2e/**/*.steps.ts'
+		],
+		format: 'pretty',
+		tags: '~@ignore'
+	},
+
   allScriptsTimeout: 11000,
-  specs: [
-    '../e2e/**/*.e2e-spec.ts'
-  ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  useAllAngular2AppRoots: true,
   beforeLaunch: function() {
     require('ts-node').register({
       project: 'e2e'
     });
   },
   onPrepare: function() {
-    jasmine.getEnv().addReporter(new SpecReporter());
+    // 
   }
 };
